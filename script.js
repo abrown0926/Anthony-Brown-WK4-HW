596d6aa71b791c57b1c3f30ea1cc33cfbcef475f
AQB <abrown0926>
2021-08-21 22:30:05 -0500

    fourth commit questions added

--- script.js
+++ script.js
@@ -120,7 +120,19 @@ var questionsObj = {
 
       { q: 'Inside which HTML element do we put the JavaScript?', options: ['<scripting>', '<script>', '<js>', '<javascript>'], answer: 2},
 
-      { q: 'How do you write "Hello World" in an alert box?', options: ['msg("Hello World")', 'alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")'], answer: 2},  
+      { q: 'How do you write "Hello World" in an alert box?', options: ['msg("Hello World")', 'alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")'], answer: 2},
+      
+      { q: 'What is the correct way to write a JavaScript array?', options: ['var colors = ["red", "green", "blue"]', 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = "red", "green", "blue"', 'var colors = (1:"red", 2:"green", 3:"blue")'], answer: 1},
+
+      { q: 'Where is the correct place to insert a JavaScript?', options: ['The <body> section', 'The <head> section', 'Both the <head> section and the <body> section', 'The <main> section'], answer: 3},
+
+      { q: 'How do you create a function in JavaScript?', options: ['Function:myFunction()', 'Function myFunction()', 'Function = myFunction()', 'Function = myFunction{}'], answer: 2},
+
+      { q: 'How to write an IF statement in JavaScript?', options: ['if i == 5 then', 'if (i == 5)', 'if i = 5', 'if i = 5 then'], answer: 2},      
+      
+      { q: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?', options: ['if i =! 5 then', 'if (i != 5)', 'if (i <> 5)', 'if i <> 5'], answer: 2},
+
+      { q: 'How does a FOR loop start?', options: ['for i = 1 to 5', 'for (i = 0; i <= 5)', 'for (i <= 5; i++)', 'for (i = 0; i <= 5; i++)'], answer: 4},
     ],
     index: 0,
     
